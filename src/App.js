408fb2b8ad18dd44a4833ae9630f63eb8311ab9e
CaptGuto <tadenafi@gmail.com>
2024-08-12 12:27:13 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        < p>
-              Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-
-        
-
+          sdf
           className="App-link"
-              href="https://reactjs.org"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
